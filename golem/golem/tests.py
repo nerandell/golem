19c347edc843c3eaa85befdf896aefba0185ec83
nerandell <ankitchandawala@gmail.com>
2015-06-28 12:52:11 +0530

    Adding another test

--- golem/golem/tests.py
+++ golem/golem/tests.py
@@ -6,8 +6,9 @@ class GolemTest(unittest.TestCase):
     def test_item_generator(self):
         name = 'TestItem'
         fields = [('username', str), ('email', str)]
-        TestItem = Golem.generate_class(name, fields)
-        self.assertEquals(TestItem._fields, [('username', str), ('email', str), ('created', int), ('updated', int)])
+        ItemClass = Golem.generate_class(name, fields)
+        self.assertTrue(ItemClass().__class__.__name__ == 'TestItem')
+        self.assertEquals(ItemClass._fields, [('username', str), ('email', str), ('created', int), ('updated', int)])
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(GolemTest)
